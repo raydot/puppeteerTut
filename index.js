bf4bd6f19c6847db2d55262c76fe0d7effd04c73
Dave Kanter <moatgoat@hotmail.com>
2018-08-21 10:25:13 -0700

    Meh, not much to be gained from going any further with this tutorial.  Perhaps I will return to it some time in the future.

--- index.js
+++ index.js
@@ -1,13 +1,36 @@
 const puppeteer = require('puppeteer');
+const CREDS = require('./creds');
 
 async function run() {
-	const browser = await puppeteer.launch();
+	// DOM element selectors
+	const USERNAME_SELECTOR = '#login_field';
+	const PASSWORD_SELECTOR = '#password';
+	const BUTTON_SELECTOR = '#login > form > div.auth-form-body.mt-3 > input.btn.btn-primary.btn-block';
+
+	const browser = await puppeteer.launch({
+		headless: false
+	});
 	const page = await browser.newPage();
 
-	await page.goto('https://github.com');
-	await page.screenshot({ path: 'screenshots/github.png' });
+	// THIS WILL TAKE A SCREENSHOT
+	//await page.goto('https://github.com/login');
+	//await page.screenshot({ path: 'screenshots/github.png' });
+
+	// THIS WILL LOG YOU IN
+	await page.click(USERNAME_SELECTOR);
+	await page.keyboard.type(CREDS.username);
+
+	await page.click(PASSWORD_SELECTOR);
+	await page.keyboard.type(CREDS.password);
+
+	await page.click(BUTTON_SELECTOR);
+
+	await page.waitForNavigation();
 
 	browser.close();
 }
 
 run();
+
+
+
